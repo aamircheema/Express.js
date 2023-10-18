50a70eae3b64073e52f36e9f4cebe67707b56d0e
Aamir Cheema <amircheema295@gmail.com>
2023-10-18 18:19:27 +0500

    Complete the CRUD api

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const uuid = require("uuid");
 const router = express.Router();
 const members = require("../../Members");
 // Get All members
@@ -21,7 +22,55 @@ router.get("/:id", (req, res) => {
 // Create Member
 
 router.post("/", (req, res) => {
-  res.send(req.body);
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  };
+  if (!newMember.name || !newMember.email) {
+    return res
+      .status(400)
+      .json({ msg: "Please include member name and email" });
+  }
+  members.push(newMember);
+  res.json(members);
+});
+
+//Update Member
+router.put("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    const updateMember = req.body;
+    members.forEach((member) => {
+      if (member.id === parseInt(req.params.id)) {
+        member.name = updateMember.name ? updateMember.name : member.name;
+        member.email = updateMember.email ? updateMember.email : member.email;
+        res.json({ msg: "Member Updated", member });
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `Member not found against id:${req.params.id}` });
+  }
+});
+
+//Delete member
+router.delete("/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json({
+      msg: "Delted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `Member not found against id:${req.params.id}` });
+  }
 });
 
 module.exports = router;
