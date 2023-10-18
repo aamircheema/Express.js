aadea04cfaf7087de8bd0446b13bbce69056ecf3
Aamir Cheema <amircheema295@gmail.com>
2023-10-18 13:26:55 +0500

    using middleware

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: "Aamir Sohail Cheema",
+    email: "aamir@gmail.com",
+    status: "Active",
+  },
+  {
+    id: 2,
+    name: "Shafiq ur Rehman Cheema",
+    email: "Shafiq@gmail.com",
+    status: "Active",
+  },
+  {
+    id: 3,
+    name: "Attiq ur Rehman Cheema",
+    email: "attiq@gmail.com",
+    status: "Active",
+  },
+  {
+    id: 4,
+    name: "Aurang Zaib Cheema",
+    email: "Zaib@gmail.com",
+    status: "Active",
+  },
+];
+
+module.exports = members;
